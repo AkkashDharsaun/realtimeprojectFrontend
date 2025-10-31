3742fbc527ce844672dbb57653905cf549e4da7c
Akkash Dharsaun <akkashdharsaun02@gmail.com>
2025-10-31 07:19:45 +0530

    Fix routes for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import './App.css'
 import Form from './Components/Form'
 import RegisterForm from './Components/Register'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
       <div>
-        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Form />} />
-            <Route path="/https://realtimeprojectfrontend-m73v.vercel.app/register" element={<RegisterForm />} />
+            <Route path="/register" element={<RegisterForm />} />
           </Routes>
-        </BrowserRouter>
       </div>
   )
 }
--- src/Components/Register/index.jsx
+++ src/Components/Register/index.jsx
@@ -49,7 +49,7 @@ const RegisterForm = () => {
   });
  const onSubmit = async (data) => {
   try {
-    const res = await fetch("http://127.0.0.1:5000/login/register", {
+    const res = await fetch("http://127.0.0.1:5000/register", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
